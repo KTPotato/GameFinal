0220d3c45a5a0f47eccd57088e83b73e593f80c6
hnjh0510 <gkstkdgns0510@naver.com>
2024-11-09 15:14:47 +0900

    플레이어

--- Assets/Scripts/HSH/Player1Ctrl.cs
+++ Assets/Scripts/HSH/Player1Ctrl.cs
@@ -11,7 +11,6 @@ public class Player1Ctrl : MonoBehaviour
     private Vector3 movedir = Vector3.zero;
     private Animation _ani;
 
-    public GameObject gun;//3인칭 마우스 y축
 
     private Ray mouseRay; //마우스 ray
     private Plane plane; //ray의 충돌을 확인할 바닥
@@ -35,11 +34,8 @@ public class Player1Ctrl : MonoBehaviour
         h = h * Mathf.Sqrt(1 - (v * v / 2));
         movedir = Vector3.forward * v + Vector3.right * h;
 
-        //3인칭
-        //FpsMove();
-
         //쿼터뷰
-        TopMove();
+        PlayerMove();
 
         //총쏘기
         if (Input.GetMouseButton(0))
@@ -47,19 +43,7 @@ public class Player1Ctrl : MonoBehaviour
             Instantiate(bullet, firePos.position, transform.rotation);
         }
     }
-
-    void FpsMove()//3인칭
-    {
-        //3인칭
-        //플레이어 이동
-        transform.Translate(movedir * Time.deltaTime * speed, Space.Self);
-        //플레이어 회전
-        transform.Rotate(Vector3.up * Time.deltaTime * rotspeed * Input.GetAxis("Mouse X"));
-        //무기 화전
-        gun.transform.Rotate(-1 * Vector3.right * Time.deltaTime * rotspeed * Input.GetAxis("Mouse Y"));
-    }
-
-    void TopMove()//탑뷰
+    void PlayerMove()//탑뷰
     {
         //플레이어 이동
         transform.position += movedir * speed * Time.deltaTime;
