e1ea00cb07590f7a64d40d36bbcb8a19fdddea2f
KTPotato <rnalgh1478069@gmail.com>
2024-12-07 21:16:55 +0900

    게임씬 건드리고 / Kang 씬 Ui 작업중

--- Assets/Scripts/KTH/SlotMachine.cs
+++ Assets/Scripts/KTH/SlotMachine.cs
@@ -13,71 +13,82 @@ public class SlotMachine : MonoBehaviour
     [System.Serializable]
     public class DisplayItemSlot
     {
-        public List<Image> SlotSprite = new List<Image> ( );
+        public List<Image> SlotSprite = new List<Image>();
     }
     public DisplayItemSlot[] DisplayItemSlots;
 
     public Image DisplayResultImage;
 
-    public List<int> StartList = new List<int> ( );
-    public List<int> ResultIndexList = new List<int> ( );
+    public List<int> StartList = new List<int>();
+    public List<int> ResultIndexList = new List<int>();
     int ItemCnt = 3;
     int[] answer = { 2, 3, 1 };
 
-    // Start is called before the first frame update
-    void Start ( )
+    // 스킬 타입과 증가 값을 저장할 딕셔너리
+    private Dictionary<int, (string skillType, float value)> skillEffects = new Dictionary<int, (string, float)>
     {
+        { 0, ("speed", 1.0f) },
+        { 1, ("firespeed", 0.1f) },
+        { 2, ("maxHp", 10.0f) },
+        { 3, ("Hp", 20.0f) },
+        { 4, ("dmg", 2.0f) },
+        { 5, ("crossfireLevel", 1) },
+        { 6, ("fan_fireLevel", 1) },
+        { 7, ("spinballLevel", 1) }
+    };
 
-        for ( int i = 0 ; i < ItemCnt * Slot.Length; i++ )
+    void Start()
+    {
+        for (int i = 0; i < ItemCnt * Slot.Length; i++)
         {
-            StartList.Add ( i );
+            StartList.Add(i);
         }
 
-        for ( int i = 0 ; i < Slot.Length; i++ )
+        for (int i = 0; i < Slot.Length; i++)
         {
-            for ( int j = 0 ; j < ItemCnt; j++ )
+            for (int j = 0; j < ItemCnt; j++)
             {
                 Slot[i].interactable = false;
 
-                int randomIndex = Random.Range ( 0, StartList.Count );
-                if ( i == 0 && j == 1 || i == 1 && j == 0 || i == 2 && j == 2 )
+                int randomIndex = Random.Range(0, StartList.Count);
+                if (i == 0 && j == 1 || i == 1 && j == 0 || i == 2 && j == 2)
                 {
-                    ResultIndexList.Add ( StartList[randomIndex] );
+                    ResultIndexList.Add(StartList[randomIndex]);
                 }
                 DisplayItemSlots[i].SlotSprite[j].sprite = SkillSprite[StartList[randomIndex]];
 
-                if ( j == 0 )
+                if (j == 0)
                 {
                     DisplayItemSlots[i].SlotSprite[ItemCnt].sprite = SkillSprite[StartList[randomIndex]];
                 }
-                StartList.RemoveAt ( randomIndex );
+                StartList.RemoveAt(randomIndex);
             }
         }
 
-        for ( int i = 0 ; i < Slot.Length; i++ )
+        for (int i = 0; i < Slot.Length; i++)
         {
-            StartCoroutine ( StartSlot ( i ) );
+            StartCoroutine(StartSlot(i));
         }
     }
-    
-    IEnumerator StartSlot ( int SlotIndex )
+
+    IEnumerator StartSlot(int SlotIndex)
     {
-        for ( int i = 0 ; i < ( ItemCnt * ( 6 + SlotIndex * 4 ) + answer[SlotIndex] ) * 2 ; i++ )
+        for (int i = 0; i < (ItemCnt * (6 + SlotIndex * 4) + answer[SlotIndex]) * 2; i++)
         {
-            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3 ( 0, 50f, 0 );
-            if ( SlotSkillObject[SlotIndex].transform.localPosition.y < 50f )
+            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 50f, 0);
+            if (SlotSkillObject[SlotIndex].transform.localPosition.y < 50f)
             {
-                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3 ( 0, 300f, 0 );
+                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3(0, 300f, 0);
             }
-            yield return new WaitForSeconds ( 0.02f );
+            yield return new WaitForSeconds(0.02f);
         }
-        for ( int i = 0 ; i < ItemCnt ; i++ )
+        for (int i = 0; i < Slot.Length; i++)
         {
             Slot[i].interactable = true;
         }
     }
 
-    public void ClickBtn ( int index )
+    public void ClickBtn(int index)
     {
         Debug.Log($"ClickBtn 호출됨: index = {index}, ResultIndexList.Count = {ResultIndexList.Count}");
 
@@ -87,6 +98,18 @@ public class SlotMachine : MonoBehaviour
             return;
         }
 
+        // 선택된 스킬을 표시
         DisplayResultImage.sprite = SkillSprite[ResultIndexList[index]];
+
+        // 선택된 스킬 효과 적용
+        if (skillEffects.TryGetValue(ResultIndexList[index], out var skillEffect))
+        {
+            PlayerData.Instance.UpdatePlayerStats(skillEffect.skillType, skillEffect.value);
+            Debug.Log($"스킬 {skillEffect.skillType}이(가) {skillEffect.value}만큼 업데이트되었습니다.");
+        }
+        else
+        {
+            Debug.LogWarning($"스킬 효과를 찾을 수 없습니다: index = {ResultIndexList[index]}");
+        }
     }
 }
---
+++ Assets/Scripts/KTH/UiController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    public static UiController Instance;
+    private static UiController instance;
+
+    public GameObject SlotMachine;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
